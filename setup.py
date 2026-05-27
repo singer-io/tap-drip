bc42e68582148c0dc1f1a0f7361fff08d09b29dc
Bryant Gray <bgray1028@gmail.com>
2026-05-27 10:32:22 -0400

    Bump requests to 2.33.0 and version to 0.0.2
    Fixes CVE-2026-25645 (twistlock security scan failure).

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name="tap-drip",
-      version="0.0.1",
+      version="0.0.2",
       description="Singer.io tap for extracting data from drip API",
       author="Stitch",
       url="http://singer.io",
@@ -9,7 +9,7 @@ setup(name="tap-drip",
       py_modules=["tap_drip"],
       install_requires=[
         "singer-python==6.8.0",
-        "requests==2.32.5",
+        "requests==2.33.0",
         "backoff==2.2.1",
       ],
       extras_require={
